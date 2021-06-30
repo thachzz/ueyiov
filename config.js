38cd757adcf49ba960c10f0d70456a0ee707ce96
thachzz <81088445+thachzz@users.noreply.github.com>
2021-06-30 21:20:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Bé yêu yêu yêu anh nhiều, nhiều nắm <33333333',
     mess: 'Anh biết mà 🥰. nhưng kệ em cơ 😘😘',
     messDesc: 'Voi Chập Chập',
-    btnAccept: 'Okiiii lun <3',
+    btnAccept: 'Kệ bé <3',
     messLink: 'https://m.me/xsxxxx',
 }
