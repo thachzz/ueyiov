807cb44226d7f7f8f0b42bbe0827722d6e60f58c
thachzz <81088445+thachzz@users.noreply.github.com>
2021-06-30 21:45:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Voi Chập ơi!',
+    introTitle: 'Chập ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,11 +9,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'yêu không.',
     btnReply: 'Gửi cho anh <3',
     reply: 'Bé yêu yêu yêu anh nhiều, nhiều nắm <33333333',
     mess: 'Anh biết mà 🥰. nhưng kệ em cơ 😘😘',
     messDesc: 'Voi Chập Chập',
     btnAccept: 'Kệ bé <3',
-    messLink: 'https://m.me/xsxxxx'
+    messLink: 'https://m.me/xsxxxx',
 }
