440e34620a8bbeec518ac99bf605da3746091872
thachzz <81088445+thachzz@users.noreply.github.com>
2025-09-25 23:31:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '<3',
+    btnIntro: 'Okay',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'yêu không.',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Bé yêu yêu yêu anh Thạch nhiều, nhiều nắm <33333333',
+    reply: 'Bé yêu yêu yêu anh thật nhiều, nhiều nắm <33333333',
     mess: 'Anh biết mà 🥰. nhưng kệ em cơ 😘😘',
     messDesc: 'Thạch Đẹp Trai',
     btnAccept: 'Kệ bé <3',
